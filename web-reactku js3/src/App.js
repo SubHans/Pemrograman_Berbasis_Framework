6e7ab868a1998f5033cc2b2b1aec7ccd219638ab
SubHans <56150593+SubHans@users.noreply.github.com>
2021-02-28 18:20:27 +0700

    Minggu 3

---
+++ web-reactku js3/src/App.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+// import Footer from './Footer';
+// import List from './List';
+import HelloComponent from './component/HelloComponent'
+
+class App extends Component{
+  render(){
+    return HelloComponent();
+  }
+}
+export default App;
---
+++ web-reactku js3/src/index.js
@@ -0,0 +1,68 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+import Login from "./Login";
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Login />
+  </React.StrictMode>,
+  document.getElementById("root")
+);
+
+reportWebVitals();
+// class Test extends React.Component{
+//   constructor(props){
+//     super(props)
+//     this.state={hello: "World!"}
+//   }
+
+//   componentWillMount(){
+//     console.log("componentWillMount()")
+//   }
+
+//   componentDidMount(){
+//     console.log("componentDidMount()")
+//   }
+
+//   changeState(){
+//     this.setState({hello : "Geek!"})
+//   }
+
+//   render(){
+//     return(
+//       <div>
+//         <h1>GeeksForGeeks.org, Hello{this.state.hello}</h1>
+//         <h2>
+//           <a onClick={this.changeState.bind(this)}>Press Here!</a>
+//         </h2>
+//       </div>
+//     )
+//   }
+
+//   shouldComponentUpdate(nextProps, nextState){
+//     console.log("shouldComponentUpdate()")
+//     return true
+//   }
+
+//   componentWillUpdate(){
+//     console.log("componentWillUpdate()")
+//     return true
+//   }
+
+//   componentDidUpdate(){
+//     console.log("componentDidUpdate()")
+//   }
+// }
+
+// ReactDOM.render(
+//     <Test/>,
+//   document.getElementById("root")
+// )
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+
