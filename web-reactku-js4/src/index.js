98a27f529ac8297c47987fe243f18bbba8b90bd5
SubHans <56150593+SubHans@users.noreply.github.com>
2021-03-08 03:57:00 +0700

    Minggu 4

---
+++ web-reactku-js4/src/index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import App from './App';
+import MahasiswaBlogPost from'./container/BlogPost/MahasiswaBlogPost';
+import reportWebVitals from './reportWebVitals';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <MahasiswaBlogPost />
+  </React.StrictMode>,
+  document.getElementById('content')
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
