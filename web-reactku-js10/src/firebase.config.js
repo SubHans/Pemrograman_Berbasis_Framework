1274913299fc3ba5c3ba6e2904ade7ad2f591f25
SubHans <56150593+SubHans@users.noreply.github.com>
2021-04-18 23:41:03 +0700

    Minggu 10

---
+++ web-reactku-js10/src/firebase.config.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+export default {
+    apiKey: "AIzaSyDZktzCG05BTFYaobIRWATEbTzX3-3f3Sk",
+    authDomain: "js10-ac99c.firebaseapp.com",
+    projectId: "js10-ac99c",
+    storageBucket: "js10-ac99c.appspot.com",
+    messagingSenderId: "90757415100",
+    appId: "1:90757415100:web:0b127e9202a6661624d0b9",
+    measurementId: "G-M6N46881M5"
+  };
