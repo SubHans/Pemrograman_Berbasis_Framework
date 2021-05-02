8a79f5959bbc1d5ab03704f299f3f3ba96f8061c
SubHans <56150593+SubHans@users.noreply.github.com>
2021-05-03 01:45:20 +0700

    minggu 12

---
+++ web-reactku-js12/src/firebase/config.js
@@ -0,0 +1,13 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyAfytPgnaFmFIWPIeq7b6cDEDWqlQKwcEQ",
+  authDomain: "js10-80956.firebaseapp.com",
+  databaseURL: "https://js10-80956-default-rtdb.firebaseio.com",
+  projectId: "js10-80956",
+  storageBucket: "js10-80956.appspot.com",
+  messagingSenderId: "173052286272",
+  appId: "1:173052286272:web:96ae8cf13c05e6a83116a5",
+  measurementId: "G-736341WCKV"
+};
+  
+  export default firebaseConfig;
+  
